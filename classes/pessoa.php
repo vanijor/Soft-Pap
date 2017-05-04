62d8c4d42467f668ecf2f1f22aabb04c38c146d1
Jorge Hélio da SIlva <vanijor@hotmail.com>
2017-05-03 23:20:12 -0300

    Add files via upload

--- classes/pessoa.php
+++ classes/pessoa.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 	/**
 	* 
 	*/
@@ -10,7 +12,7 @@
 		private $Salario;
 		private $Login;
 		private $Senha;
-
+		
 
 		function __construct($Nome='', $Telefone='',$Endereco='', $Salario='', $Login='', $Senha='', $RG='', $Adm='',$Cpf='')
 		{
@@ -23,69 +25,156 @@
 			$this->RG = $RG;
 			$this->Adm = $Adm;
 			$this->Cpf = $Cpf;
+
 		}
 
-		public function getNome(){
-			return $this->Nome;
-		}
-		public function setNome($Nome){
-			$this->Nome = $Nome;
+		public function __get($atributo)
+		{
+			return $this->atributo;
 		}
 
-		public function getTelefone(){
-			return $this->Telefone;
-		}
-		public function setTelefone($Telefone){
-			$this->Telefone = $Telefone;
+		public function __set($atributo, $valor)
+		{
+			$this->atributo = $valor;
 		}
 
-		public function getEndereco(){
-			return $this->Endereco;
-		}
-		public function setEndereco($Endereco){
-			$this->Endereco = $Endereco;
+
+		// INSERE TODAS AS COLUNAS DA TABELA PESSOA
+		public function insert($InputNome, $InputTelefone, $InputEndereco, $InputSalario, $InputLogin, $InputSenha, $InputRG, $InputCpf, $InputAdm){
+
+			include("dbconnect.php");
+
+			$query = "INSERT INTO pessoa (nm_nome, cd_telefone, ds_endereco, vl_salario, cd_login, cd_senha, cd_rg, cd_cpf, cd_adm) VALUES (?,?,?,?,?,?,?,?,?)";
+
+			$stmt = $db_conn->prepare($query);
+			$stmt->bind_param("sisdssiii", $InputNome, $InputTelefone, $InputEndereco, $InputSalario, $InputLogin, $InputSenha, $InputRG, $InputCpf, $InputAdm);
+            	# corrigir os nomes aqui, e os setters embaixo.
+			if ($stmt->execute()){
+				$this->Nome = $InputNome;
+				$this->Telefone = $InputTelefone;
+				$this->Endereco = $InputEndereco;
+				$this->Salario = $InputSalario;
+				$this->Login = $InputLogin;
+				$this->Senha = $InputSenha;
+				$this->RG = $InputRG;
+				$this->Adm = $InputAdm;
+				$this->Cpf = $InputCpf;
+
+			echo '<script>window.location="painel_list_user.php";</script>';
+			$stmt->close();
+			$db_conn->close();
+			return true; // Execução com sucesso
+			}
+		print_r($stmt->error);
+		print_r($stmt->errno);
+		$stmt->close();
+		$db_conn->close();
+        return false; // Erro na execução
+        }
+
+
+
+		// CONSULTA E EXIBE TODAS AS LINHAS DA TABELA PESSOA 
+        public function select(){
+
+			include("dbconnect.php");
+
+        	$db_conn = mysqli_connect("localhost","root","","u573658764_papel") 
+        	or
+        	die("Não foi possível conectar:" . mysqli_connect_errno());
+
+        	$query = "SELECT cd_pessoa, nm_nome, cd_rg, cd_cpf, cd_telefone, ds_endereco, vl_salario, cd_adm FROM pessoa";
+
+
+        	$stmt = $db_conn->prepare($query);
+        	if($stmt->execute()) {
+        		$stmt->bind_result($cd_pessoa, $nm_nome, $cd_rg, $cd_cpf, $cd_telefone, $ds_endereco, $vl_salario, $cd_adm);
+
+				// EXIBE PESSOA
+        		while($stmt->fetch()) {
+        			printf("<tr>");
+        			printf("<td>" .
+        				$cd_pessoa
+        				. "</td><td>" . 
+        				$nm_nome
+        				. "</td><td>" . 
+        				$cd_rg
+        				. "</td><td>" . 
+        				$cd_cpf
+        				. "</td><td>" . 
+        				$cd_telefone
+        				. "</td><td>" . 
+        				$ds_endereco
+        				. "</td><td>" .
+        				$vl_salario
+        				. "</td><td>" .
+        				$cd_adm
+        				. "</td><td>" .
+        				"<button onclick="."confirm_modal('deletar.php?cd_pessoa=".$cd_pessoa."')> APAGAR </button>"
+        				. " | " .
+        				"<button href='#update_modal' data-id=".$cd_pessoa." data-toggle='modal' data-target='#update_modal' class='modalLink'> ALTERAR </button>"
+        				);
+        			printf("<tr>");
+        			}
+        	$stmt->close();
+        	$db_conn->close();
+			return true; // Execução com sucesso
+			}
+		$stmt->error;
+		$stmt->errno;
+		$stmt->close();
+		$db_conn->close();
+		return false; // Erro na execução
 		}
 
-		public function getSalario(){
-			return $this->Salario;
-		}
-		public function setSalario($Salario){
-			$this->Salario = $Salario;
+
+		// ALTERA DADOS DA LINHA SELECIONADA
+	public function update($InputNome, $InputTelefone, $InputEndereco, $InputSalario, $InputLogin, $InputSenha, $InputRG, $InputCpf, $InputAdm, $Id){
+
+			include("dbconnect.php");
+
+			$query = "UPDATE Pessoa SET nm_nome = ?, cd_telefone = ?, ds_endereco = ?, vl_salario = ?, cd_login = ?, cd_senha = ?, cd_rg = ?, cd_cpf = ?, cd_adm = ? WHERE cd_pessoa = ?";
+
+			$stmt = $db_conn->prepare($query);
+
+			$stmt->bind_param("sisdssiiii", $InputNome, $InputTelefone, $InputEndereco, $InputSalario, $InputLogin, $InputSenha, $InputRG, $InputCpf, $InputAdm, $Id);
+
+			if($stmt->execute()) {
+				$stmt->close();
+				$db_conn->close();
+				echo '<script>window.location="painel_list_user.php";</script>';
+			return true; // Execução com sucesso
+			}
+
+		$stmt->error;
+		$stmt->errno;
+		$stmt->close();
+		$db_conn->close();
+		echo "A alteração falhou";
+		return false; // Erro na execução
 		}
 
-		public function getLogin(){
-			return $this->Login;
-		}
-		public function setLogin($Login){
-			$this->Login = $Login;
+	public function delete($Id){
+
+			include("dbconnect.php");
+
+			$query = "DELETE FROM pessoa WHERE cd_pessoa = ?";
+			$stmt = $db_conn->prepare($query);
+			$stmt->bind_param("i", $Id);
+
+			if($stmt->execute()) {
+				$stmt->close();
+				$db_conn->close();
+				echo '<script>window.location="painel_list_user.php";</script>';
+			return true; // Execução com sucesso
+			}
+
+		$stmt->error;
+		$stmt->errno;
+		$stmt->close();
+		$db_conn->close();
+		return false; // Erro na execução
 		}
 
-		public function getSenha(){
-			return $this->Senha;
-		}
-		public function setSenha($Senha){
-			$this->Senha = $Senha;
-		}
-
-		public function getRG(){
-			return $this->RG;
-		}
-		public function setRG($RG){
-			$this->RG = $RG;
-		}
-
-		public function getAdm(){
-			return $this->Adm;
-		}
-		public function setAdm($Adm){
-			$this->Adm = $Adm;
-		}
-
-				public function getCpf(){
-			return $this->Cpf;
-		}
-		public function setCpf($Cpf){
-			$this->Cpf = $Cpf;
-		}
-	}
+}
 ?>
